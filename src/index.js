858f8da56d08f7c61eab400f4d6393783c651ee5
masone85 <113108800+masone85@users.noreply.github.com>
2024-01-12 18:57:39 +0000

    added basic form handling and typewriter plug in

--- src/index.js
+++ src/index.js
@@ -1,1 +1,12 @@
-//alert("hello world");
+function generateOutput(event) {
+  event.preventDefault();
+
+  new Typewriter("#output-display", {
+    strings: "Hello Nanny",
+    autoStart: true,
+    cursor: "",
+  });
+}
+
+let outputGeneratorElement = document.querySelector("#output-generator-form");
+outputGeneratorElement.addEventListener("submit", generateOutput);
